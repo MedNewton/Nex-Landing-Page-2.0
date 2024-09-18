a2f62a244db1fd15ee2d3e72a1a93e186e9a11de
MedNewton <med.newton.2013@gmail.com>
2024-09-18 15:58:45 +0200

    restyles spot index section title

--- src/components/IndexModel/indexModel.tsx
+++ src/components/IndexModel/indexModel.tsx
@@ -9,10 +9,9 @@ import xmodel from '@assets/images/XLogoModel.png'
 const SpotIndexModel = () => {
     return (
         <section className="w-full h-fit flex flex-col items-center justify-start p-6 gap-6">
-            <h5 className="text-3xl font-semibold">Spot Index Model</h5>
             <div className="w-full h-fit flex flex-row items-stretch justify-between relative bg-nexLightCard-500 dark:bg-nexDarkCard-500 shadow-lg dark:shadow-none rounded-xl p-4 gap-4  overflow-hidden">
                 <div className="w-3/5 h-fit left-0 top-0 flex flex-col items-start justify-start p-4 gap-2">
-                    <h5 className="text-3xl font-semibold">Diversify with Confidence</h5>
+                    <h5 className="text-3xl font-semibold">Nex Spot Index Model: Diversify with Confidence</h5>
                     <h6 className="text-xl font-medium flex flex-row items-center"> The Spot Index Standard Model for Portfolio Enhancement</h6>
                     <p className="text-base w-11/12 leading-relaxed">
                         The Spot Index Standard Model aims to enable investors to diversify their portfolios by investing in an asset index rather than a single cryptocurrency.
@@ -20,15 +19,15 @@ const SpotIndexModel = () => {
                         Each index token is supported by collateral stored securely in a custodian wallet, reminiscent of wBTC backed by Bitcoin.
                     </p>
                     <h6 className="text-lg font-bold flex flex-row items-center">Key features:</h6>
-                    <div className="w-full grid grid-cols-2 gap-6">
+                    <div className="grid grid-cols-2 gap-6 w-11/12">
                         <div className="h-fit flex flex-col items-start justify-start gap-2">
-                            <h6 className="text-lg font-medium">Automated Rebalancing of Asset Portfolios</h6>
+                            <h6 className="text-lg font-medium w-10/12">Automated Rebalancing of Asset Portfolios</h6>
                             <p className="text-base w-11/12 leading-relaxed">
                                 Effortlessly keep your portfolio aligned with your goals as our system adjusts asset allocations automatically.
                             </p>
                         </div>
                         <div className="h-fit flex flex-col items-start justify-start gap-2">
-                            <h6 className="text-lg font-medium">Market Cap-Driven Weighting for Underlying Assets</h6>
+                            <h6 className="text-lg font-medium w-10/12">Market Cap-Driven Weighting for Underlying Assets</h6>
                             <p className="text-base w-11/12 leading-relaxed">
                             Ensure accurate asset distribution with weights based on market capitalization, reflecting true market value.
                             </p>
